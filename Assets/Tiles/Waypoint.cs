02e256b6c66b25902639ca4358bfe57a0adeb2a0
Joon <58021684+wonjune2@users.noreply.github.com>
2024-04-17 00:18:31 +0900

    0417

---
+++ Assets/Tiles/Waypoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
